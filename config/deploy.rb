4f8d2a9ac1ec9909716d4a8dad217f40c542e804
Aleksey Pohodnya <pohodnyaa@gmail.com>
2016-03-10 11:56:25 +0600

    Config Deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,78 +7,47 @@ set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
-# Don't change these unless you know what you're doing
-set :pty,             true
-set :use_sudo,        false
-set :stage,           :production
-set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
-## Defaults:
-# set :scm,           :git
-# set :branch,        :master
-# set :format,        :pretty
-# set :log_level,     :debug
-# set :keep_releases, 5
+# config valid only for current version of Capistrano
+lock '3.4.0'
 
-## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :application, 'transport'
+set :repo_url, 'git@github.com:pohodnya/transport.git'
+set :sidekiq_pid, File.join(shared_path, 'pids', 'sidekiq.pid')
 
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
+
+if File.exist?("./config/deploy_id_rsa")
+  puts "file exist"
+  set :ssh_options, keys: ["./config/deploy_id_rsa"]
+else
+  set :password, ask('Server password:', nil, echo: false)
+end
+set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'pids',  'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+
+after 'deploy:publishing', 'deploy:restart'
+before 'deploy:assets:precompile', 'bower:install'
+after 'deploy:assets:precompile', 'deploy:cleanup_assets'
+after 'deploy:assets:precompile', 'ember:copy_assets'
+
+namespace :bower do
+  desc 'Install bower components'
+  task :install do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      execute "cd #{release_path}/frontend && bower install"
     end
   end
-
-  before :start, :make_dirs
 end
 
 namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
+  after :restart, :db_migrate do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      within release_path do
+        with rails_env: "#{fetch(:stage)}" do
+          execute :rake, 'db:migrate'
+        end
       end
     end
   end
-
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
-  end
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
-
-  before :starting,     :check_revision
-  after  :finishing,    :compile_assets
-  after  :finishing,    :cleanup
-  after  :finishing,    :restart
 end
-
-# ps aux | grep puma    # Get puma pid
-# kill -s SIGUSR2 pid   # Restart puma
-# kill -s SIGTERM pid   # Stop puma
